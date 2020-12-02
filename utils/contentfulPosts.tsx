3f7503dcd267ccc59786d03253403aced566af24
Fernando Montero <fernando.montero.perez@gmail.com>
2020-12-02 20:49:15 +0000

    fix netlify.toml

--- utils/contentfulPosts.tsx
+++ utils/contentfulPosts.tsx
@@ -1,4 +1,4 @@
-import Contentful, { Entry, EntryCollection } from "contentful";
+import * as contentful from "contentful";
 
 const space = process.env.NEXT_PUBLIC_CONTENTFUL_SPACE_ID;
 const accessToken = process.env.NEXT_PUBLIC_CONTENTFUL_ACCESS_TOKEN;
@@ -6,31 +6,31 @@ const accessToken = process.env.NEXT_PUBLIC_CONTENTFUL_ACCESS_TOKEN;
 /*
  * https://contentful.github.io/contentful.js/contentful/7.15.1/
  */
-const client = Contentful.createClient({
+const client = contentful.createClient({
   space: space,
   accessToken: accessToken,
 });
 
 const PostsService = {
-  async fetchEntries(): Promise<EntryCollection<any>> {
+  async fetchEntries() {
     const entries = await client.getEntries({
       content_type: "post",
     });
     if (entries.items) return entries.items;
   },
 
-  async getPostById(id: String): Promise<Entry<any>> {
+  async getPostById(id: string) {
     const entry = await client.getEntry(id);
     if (entry.sys) return entry;
   },
 
-  async getPostBySlug(slug: String): Promise<Entry<any>> {
+  async getPostBySlug(slug: string) {
     const entry = await client.getEntries({
       content_type: "post",
       "fields.slug[match]": slug,
       include: 1,
     });
-    if (entry.sys) return entry.items[0];
+    return entry.items[0];
   },
 };
 
