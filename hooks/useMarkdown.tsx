1d434f6ccebaa41437c9c32018212cf6aa96367d
Fernando Montero <fernando.montero.perez@gmail.com>
2020-12-03 09:26:11 +0000

    feat: Refactored Markdown processing into useMarkdown hook

---
+++ hooks/useMarkdown.tsx
@@ -0,0 +1,53 @@
+import React, { useEffect, useState } from "react";
+import marked, { Renderer } from "marked";
+import { renderToString } from "react-dom/server";
+import Link from "next/link";
+import Icon from "@mdi/react";
+import { mdiOpenInNew } from "@mdi/js";
+import Title from "@components/atoms/Title";
+import * as prism from "prismjs";
+import DOMPurify from "dompurify";
+
+const useMarkdown = (markdown: string) => {
+  const [html, setHtml] = useState("");
+  useEffect(() => {
+    const renderer: Renderer = ({
+      link(href, title, text) {
+        return renderToString(
+          <Link href={href}>
+            <a
+              href={href}
+              className="inline-flex items-end text-primary-main underline"
+            >
+              {text}
+              <Icon path={mdiOpenInNew} size={0.75} />
+            </a>
+          </Link>
+        );
+      },
+      heading(text, level) {
+        const escapedText = text.toLowerCase().replace(/[^\w]+/g, "-");
+        return renderToString(
+          <Title size={level}>
+            <>
+              <a className="anchor" href={`#${escapedText}`}>
+                <span className="mr-2">#</span>
+              </a>
+              {text}
+            </>
+          </Title>
+        );
+      },
+    } as unknown) as Renderer;
+    marked.use({ renderer });
+    setHtml(DOMPurify.sanitize(marked(markdown)));
+  }, []);
+  useEffect(() => {
+    if (html === "") return;
+    prism.highlightAll();
+  }, [html]);
+
+  return html;
+};
+
+export default useMarkdown;
--- pages/blog/[id].tsx
+++ pages/blog/[id].tsx
@@ -1,56 +1,13 @@
 import React, { Fragment, useEffect, useState } from "react";
 import Head from "next/head";
-import Link from "next/link";
 import PostsService from "@utils/contentfulPosts";
 import { GetServerSideProps } from "next/types";
 import PageContainer from "@components/template/PageContainer";
-import Title from "@components/atoms/Title";
-import marked, { Renderer } from "marked";
-import * as prism from "prismjs";
-import { renderToString } from "react-dom/server";
 import TransparentHero from "@components/template/hero/TransparentHero";
-import Icon from "@mdi/react";
-import { mdiOpenInNew } from "@mdi/js";
-import DOMPurify from "dompurify";
+import useMarkdown from "../../hooks/useMarkdown";
 
 export default function PostPage({ post }) {
-  const [html, setHtml] = useState("");
-  useEffect(() => {
-    const renderer: Renderer = ({
-      link(href, title, text) {
-        return renderToString(
-          <Link href={href}>
-            <a
-              href={href}
-              className="inline-flex items-end text-primary-main underline"
-            >
-              {text}
-              <Icon path={mdiOpenInNew} size={0.75} />
-            </a>
-          </Link>
-        );
-      },
-      heading(text, level) {
-        const escapedText = text.toLowerCase().replace(/[^\w]+/g, "-");
-        return renderToString(
-          <Title size={level}>
-            <>
-              <a className="anchor" href={`#${escapedText}`}>
-                <span className="mr-2">#</span>
-              </a>
-              {text}
-            </>
-          </Title>
-        );
-      },
-    } as unknown) as Renderer;
-    marked.use({ renderer });
-    setHtml(DOMPurify.sanitize(marked(post.fields.body)));
-  }, []);
-  useEffect(() => {
-    if (html === "") return;
-    prism.highlightAll();
-  }, [html]);
+  const html = useMarkdown(post.fields.body);
 
   return (
     <>
